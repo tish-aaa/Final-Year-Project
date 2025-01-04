1c79d8a44c88da8a9b9663ac9326c8f3c4e004ed
Tisha Sharma <124681335+tish-aaa@users.noreply.github.com>
2025-01-04 23:38:56 +0530

    linked login and signup pages to index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return <Stack />;
+  return(
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='index' />
+      <Stack.Screen name='signup' />
+      <Stack.Screen name='login' />
+      <StatusBar backgroundColor='white' style='dark' />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import MyButton from "@/app-example/components/MyButton";
+import { StyleSheet, Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+  const onLogin = () => {
+    router.navigate("/login")
+  };
+  const onSignup = () => {
+    router.navigate("/signup")
+  };
   return (
     <View
       style={{
         flex: 1,
+        gap: 20,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.titleText}>Hello there, Welcome Back!</Text>
+      <MyButton title={"Login"} onPress={onLogin}/>
+      <Text style={styles.text}>Not signed up yet? Click below! </Text>
+      <MyButton title={"SignUp"} onPress={onSignup}/>
     </View>
   );
 }
+
+
+// STYLING
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 24,
+    fontWeight: 700,
+    textAlign: "center"
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 500,
+    textAlign: "center",
+    marginTop: 20,
+}
+});
