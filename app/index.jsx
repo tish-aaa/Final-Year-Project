6488c684e469786fc6fbae89f0099562f38f3d03
Tisha Sharma <124681335+tish-aaa@users.noreply.github.com>
2025-01-13 00:11:54 +0530

    login+signup ui added

---
+++ app/index.jsx
@@ -0,0 +1,44 @@
+import MyButton from "@/app-example/components/MyButton";
+import { StyleSheet, Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+
+export default function Index() {
+  const router = useRouter();
+  const onLoginBtnClick = () => {
+    router.navigate("/login");
+  };
+  const onSignupBtnClick = () => {
+    router.navigate("/signup");
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        gap: 20,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={styles.titleText}>Hello there, Welcome Back!</Text>
+      <MyButton title={"Login"} onPress={onLoginBtnClick} />
+      <Text style={styles.text}>Not signed up yet? Click below! </Text>
+      <MyButton title={"Sign Up"} onPress={onSignupBtnClick} />
+    </View>
+  );
+}
+
+// STYLING
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 24,
+    fontWeight: 700,
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 500,
+    textAlign: "center",
+    marginTop: 20,
+  },
+});
---
+++ app/signup.jsx
@@ -0,0 +1,43 @@
+import { View, Text, Image, TextInput } from 'react-native'
+import MyButton from "@/app-example/components/MyButton";
+import { Link, useRouter } from "expo-router";
+
+const SignUp = () => {
+    const router = useRouter();
+    const onSignUp = () => {
+        router.navigate("/");
+    }
+    return (
+        <View
+            style={{
+                flex: 1
+            }}>
+            <Image source={require("@/assets/images/signup_img.jpg")} style={{
+                width: "100%", height: 400,
+            }}
+                resizeMode='cover'
+            />
+            <View style={{ flex: 1, padding: 20, gap: 20, alignItems: "center" }}>
+                <TextInput placeholder="Enter Your Email"
+                    style={{ borderWidth: 1, height: 50, paddingHorizontal: 20, borderRadius: 5, width: "100%" }}
+                    onChangeText={(e) => console.log(e)}
+                />
+                <TextInput placeholder="Enter Your Email"
+                    style={{ borderWidth: 1, height: 50, paddingHorizontal: 20, borderRadius: 5, width: "100%" }}
+                    onChangeText={(e) => console.log(e)}
+                />
+                <TextInput placeholder="Enter Your Password"
+                    style={{ borderWidth: 1, height: 50, paddingHorizontal: 20, borderRadius: 5, width: "100%" }}
+                    onChangeText={(e) => console.log(e)}
+                />
+                <TextInput placeholder="Re-Enter Your Password"
+                    style={{ borderWidth: 1, height: 50, paddingHorizontal: 20, borderRadius: 5, width: "100%" }}
+                    onChangeText={(e) => console.log(e)}
+                />
+                <MyButton title={"Register Now"} onPress={onSignUp} />
+            </View>
+        </View>
+    )
+}
+
+export default SignUp;
